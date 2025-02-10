6bab9a2b653b05887b50395244994916674a6198
davims9 <davimourasouza@gmail.com>
2025-02-09 21:45:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# U4C6-interfaces
+ tarefa da unidade 4 capitulo 6 do Embarcatech
---
+++ U4C6-interfces.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "hardware/uart.h"
+#include "hardware/i2c.h"
+#include "pico/bootrom.h"
+#include "lib/ws2812.pio.h"
+#include "lib/ssd1306.h"
+
+int main() {
+
+    //loop da função princiapal
+    while(true) {
+
+    }
+}
